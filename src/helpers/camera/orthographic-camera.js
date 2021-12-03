ab15b7e828bc7727fc68c48ba6593dcdf5b80297
Miguel Malqui <miquelangel9924@gmail.com>
2021-12-03 22:36:55 +0100

    Add comments

--- src/helpers/camera/orthographic-camera.js
+++ src/helpers/camera/orthographic-camera.js
@@ -1,20 +1,50 @@
 import Matrix4x4 from "../maths/matrix4x4.js";
 import Camera from "./camera.js";
 
-export default class PerspectiveCamera extends Camera {
+/**
+ * Class representing a orthografic camera
+ */
+export default class OrthograficCamera extends Camera {
 
-    constructor(left, right, bottom, top, zNear, zFar) {
+    /**
+     * Creates a orthografic camera
+     * @param {number} left - camera frustum left plane, default is -1
+     * @param {number} right - camera frustum right plane, default is 1
+     * @param {number} bottom - camera frustum bottom plane, default is -1
+     * @param {number} top - camera frustum top plane, default is 1
+     * @param {number} [zNear] - the minimal distance between the camera and a 
+     * visible surface, default is 0.1
+     * @param {number} [zFar] - the maximal distance between the camera and a
+     * visible surface, default is 100
+     */
+    constructor(left = -1, right = 1, bottom = -1, top = 1, zNear, zFar) {
         super(zNear, zFar);
 
+        /**
+         * camera frustum left plane
+         * @type {number}
+         */
         this.left = left;
+        /**
+         * camera frustum right plane
+         * @type {number}
+         */
         this.right = right;
+        /**
+         * camera frustum bottom plane
+         * @type {number}
+         */
         this.bottom = bottom;
+        /**
+         * camera frustum top plane
+         * @type {number}
+         */
         this.top = top;
 
-        this.updateCamera();
+        this.updateProjectionMatrix();
     }
 
-    updateCamera() {
+    updateProjectionMatrix() {
         this.projectionMatrix = Matrix4x4.orthogonal(
             this.left, this.right, this.bottom, this.top,
             this.zNear, this.zFar
--- src/helpers/camera/perspective-camera.js
+++ src/helpers/camera/perspective-camera.js
@@ -1,12 +1,33 @@
 import Matrix4x4 from "../maths/matrix4x4.js";
 import Camera from "./camera.js";
 
+/**
+ * Class representing a perspective camera
+ */
 export default class PerspectiveCamera extends Camera {
-    
-    constructor(fov, ra, zNear, zFar) {
+
+    /**
+     * Creates a perspective camera
+     * @param {number} fov - camera frustum vertical field of view, in radians, 
+     * default is 1.0
+     * @param {number} ra - aspect ratio, default is 1.0
+     * @param {number} [zNear] - the minimal distance between the camera and a 
+     * visible surface, default is 0.1
+     * @param {number} [zFar] - the maximal distance between the camera and a
+     * visible surface, default is 100
+     */
+    constructor(fov = 1.0, ra = 1.0, zNear, zFar) {
         super(zNear, zFar);
 
+        /**
+         * camera frustum vertical field of view, in radians
+         * @type {number}
+         */
         this.fov = fov;
+        /**
+         * aspect ratio
+         * @type {number}
+         */
         this.ra = ra;
 
         this.updateProjectionMatrix();
